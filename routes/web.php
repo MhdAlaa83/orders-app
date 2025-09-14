d81f6b79ca6faf41ed0009395034251388fa9a49
MIS-ShSR <mhd.alaa.allawi@gmail.com>
2025-09-14 19:32:57 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 
 /*
+
 |----------------------------------------------
 | Orders Page
 |----------------------------------------------
 | GET /orders -> OrderController@index
 | Route name: orders.index (conventional)
+
 */
 Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
